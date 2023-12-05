786c7b600c1e77eea51a76b35ce5de0eb112ced3
alaricli <alaricli@outlook.com>
2023-12-04 18:48:22 -0800

    add contact links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { BlogComponent } from './blog/blog.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
   },
-  {
-    path: 'about',
-    component: AboutComponent
-  }, 
   {
     path: 'projects',
     component: ProjectsComponent
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -19,9 +19,10 @@ import { Component } from '@angular/core';
           Home
         </a>
 
-        <a class="navbar-item" routerLink="/about">
+        <!-- About page not necessarily atm -->
+        <!-- <a class="navbar-item" routerLink="/about">
           About
-        </a>
+        </a> -->
 
         <a class="navbar-item" routerLink="/projects">
           Projects
@@ -37,7 +38,19 @@ import { Component } from '@angular/core';
     <div class="navbar-end">
       <div class="navbar-item">
         <div class="buttons">
-          <a class="button" routerLink="/contact">
+          <a 
+            class="button"
+            href="https://drive.google.com/file/d/1JiSXdsosY6YDDvIFjEWdbxa0FGPa2_ZA/view?usp=sharing"
+          >
+            Resume
+          </a>
+          <!-- Replace example@example.com with email address in production -->
+          <a 
+            class="button"
+            href="mailto:example@example.com?subject=Hello%20Alaric&body=Hello%20Alaric,"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <strong>Contact</strong>
           </a>
         </div>
